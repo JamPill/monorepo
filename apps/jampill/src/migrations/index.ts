d4c1a1688f1cd210726873a32780a0f033b3bb06
SergioPisoni <sergio.pisoni@me.com>
2026-01-02 01:51:20 +0100

    feat: aggiunta la migrazione dello schema iniziale del database e configurazione del tipo di modulo.

--- apps/jampill/src/migrations/index.ts
+++ apps/jampill/src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250929_111647 from './20250929_111647'
+import * as migration_20250929_111647 from './20250929_111647';
+import * as migration_20260102_004631_initial_schema from './20260102_004631_initial_schema';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250929_111647.down,
     name: '20250929_111647',
   },
-]
+  {
+    up: migration_20260102_004631_initial_schema.up,
+    down: migration_20260102_004631_initial_schema.down,
+    name: '20260102_004631_initial_schema'
+  },
+];
